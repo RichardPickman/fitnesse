aa5e6ea2afcf1577717173e78081a3ec0b100a03
propertyofrichardpickman <mentaporn@gmail.com>
2026-07-27 18:18:36 +0200

    008: Replace crypto.randomUUID with RN-compatible UUID

--- src/db/plans.ts
+++ src/db/plans.ts
@@ -52,7 +52,10 @@ function hashPlanContent(name: string, days: number[]): string {
 }
 
 function newId(): string {
-  return crypto.randomUUID();
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
 }
 
 // ---------------------------------------------------------------------------
